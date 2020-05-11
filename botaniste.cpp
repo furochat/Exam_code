b3ad01b533bb98a03d89a8ab56ec9bfced0ef7a2
furochat <59836300+furochat@users.noreply.github.com>
2020-05-11 17:46:34 +0200

    Create botaniste.cpp

---
+++ botaniste.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include "botaniste.hpp"
+
+main ()
+{
+    int engrais = 10;
+    int argent = 100;
+    int jour = 0;
+
+
+
+
+    return 0;
+}
+void dormir () // passe tour +1 jours
+void tailler ()
+void arroser () // 1x/jours et plante
+void vendre ()
+void acheterEngrais ()
+void acheterPlante ()
+void nourrirPlante ()
+void vendrePlante ()
+void examinerPlante ()
