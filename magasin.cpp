25d138cedbf79ce70eaae35561b3352eed102908
furochat <59836300+furochat@users.noreply.github.com>
2020-05-11 17:48:07 +0200

    Create magasin.cpp

---
+++ magasin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include "magasin.hpp"
+
+
+main ()
+{   
+
+    int tab[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        for(int i{} ; i < 2; ++i)
+            std::cout << tab[i] << std::endl;
+
+        return 0;
+}
