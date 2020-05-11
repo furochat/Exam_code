e01d2b088f9b571f3b6b05a2aa91d2ad81b1271a
furochat <59836300+furochat@users.noreply.github.com>
2020-05-11 17:45:20 +0200

    Create plantes.cpp

---
+++ plantes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include "plantes.hpp"
+
+void bonneCroissance () //influencer par santé, taille, quand joueur passe un tour
+{
+    std::cout << Croissance + << std::endl;
+}
+void empoisonner () // trop d'engrais
+{
+    std::cout << empoisonnée :[ << std::endl;
+}
+void noyade () //trop arroser
+{
+    std::cout << noyée :[ << std::endl;
+} 
+void trancher ()//trop taillé
+    std::cout << << std::endl;
+ //trop taillé
+
+/* santé s'améliore+ par arrosage, taillage,
+détérotation -noyade, empoisonner 
+*/
+main ()
+{
+ int sante = 100;
+ int taille = 0;
+ string nom;
+// incrémentation +1 dès que joueur dort passe sont tour.
+ int age = 0;
+
+return 0;
+}
+fleur ()
+{
+    //croissance x0.75 prévoir float
+}
+planteCarnivore ()
+{
+    //croissance x1 avec engrais + mouche x1.25
+}
