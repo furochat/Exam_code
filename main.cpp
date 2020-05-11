36fddd4053a7b763fb2af7d3d4a2e5647fc34d3f
furochat <59836300+furochat@users.noreply.github.com>
2020-05-11 17:44:24 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include "botaniste.hpp"
+#include "magasin.hpp"
+#include "plantes.hpp"
+
+int main ()
+{
+
+int jour = 0;
+
+return 0;
+}
